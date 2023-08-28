c0267c2f8dfbc66cf91dc1255db14ca641ef0aca
fawn <fawn@envs.net>
2023-08-28 21:58:49 +0300

    doesn't even

--- src/tty.rs
+++ src/tty.rs
@@ -22,7 +22,7 @@ mod unix {
         unsafe { (tcsetattr(0, TCSAFLUSH, &p) != -1).then_some(()) }
     }
 
-    pub fn make_raw(p: Params) -> Params {
+    pub const fn make_raw(p: Params) -> Params {
         let mut new = p;
         unsafe { cfmakeraw(&mut new) };
         // keep OPOST so we don't need to do \r\n manually
@@ -42,7 +42,43 @@ pub use unix::*;
 // TODO: consoleapi equivalent
 
 #[cfg(windows)]
-mod windows {}
+mod windows {
+    use winapi::{
+        shared::minwindef::DWORD,
+        um::{
+            consoleapi::{GetConsoleMode, SetConsoleMode},
+            processenv::GetStdHandle,
+            winbase::STD_OUTPUT_HANDLE,
+            wincon::{
+                ENABLE_ECHO_INPUT, ENABLE_EXTENDED_FLAGS, ENABLE_INSERT_MODE, ENABLE_LINE_INPUT,
+                ENABLE_PROCESSED_INPUT, ENABLE_QUICK_EDIT_MODE, ENABLE_WINDOW_INPUT,
+            },
+        },
+    };
+
+    pub type Params = DWORD;
+
+    pub fn get_params() -> Option<Params> {
+        let mut p: Params = 0;
+        unsafe { (GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &mut p) != -1).then_some(p) }
+    }
+
+    pub fn set_params(p: Params) -> Option<()> {
+        unsafe { (SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), p) != -1).then_some(()) }
+    }
+
+    pub const fn make_raw(p: Params) -> Params {
+        let mut new = p;
+        new &= !(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
+
+        new |= ENABLE_EXTENDED_FLAGS;
+        new |= ENABLE_INSERT_MODE;
+        new |= ENABLE_QUICK_EDIT_MODE;
+        new |= ENABLE_WINDOW_INPUT;
+
+        new
+    }
+}
 
 #[cfg(windows)]
 pub use windows::*;
